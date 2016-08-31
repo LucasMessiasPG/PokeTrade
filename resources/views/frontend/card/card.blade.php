5095ae653f37f63e009a041aeb6e3ea4d7231361
lucasmessiaspg <ml23110405>
2016-08-31 14:38:20 -0300

    loading mobile

--- resources/views/frontend/card/card.blade.php
+++ resources/views/frontend/card/card.blade.php
@@ -1,20 +1,37 @@
-<div class="info-card">
-    <div>
-        <h4 class="center-align"><a>Nome da carta (#12/123)</a></h4>
-        <ul>
-            <li>Coleção: <strong>Fate Colide</strong></li>
-            <li>Raridade: <strong>Uncomon</strong></li>
-            <li>Trainer: <a>Messias</a></li>
-            <li>PokePoint: <strong>1313</strong></li>
-        </ul>
-        <ul class="button hide-on-small-and-down">
-            <li><button class="btn btn-small">+Desejo</button></li>
-            <li><button class="btn btn-small">+Tenho</button></li>
-            <li><button class="btn btn-small">Enviar</button></li>
-        </ul>
-        <button class="card-button"></button>
+<div class="col s12 l6">
+    <div class="img-card valign-wrapper">
+        <div>
+            <img class="valign materialboxed" src="/img/40.png" alt="">
+        </div>
     </div>
 </div>
-<div class="img-card valign-wrapper">
-    <img class="valign materialboxed" src="/img/40.png" alt="">
+<div class="col s12 l6">
+    <div class="info-card">
+        <div>
+            <div class="col s12">
+                <h4 class="center-align"><a>Nome da carta (#12/123)</a></h4>
+            </div>
+            <div class="col s8 l12">
+                <ul>
+                    <li>Coleção: <strong>Fate Colide</strong></li>
+                    <li>Raridade: <strong>Uncomon</strong></li>
+                    <li>Trainer: <a>Messias</a></li>
+                    <li>PokePoint: <strong>1313</strong></li>
+                </ul>
+            </div>
+            <div class="col s4 l12">
+                <ul class="button">
+                    <li>
+                        <button class="btn btn-small">+Desejo</button>
+                    </li>
+                    <li>
+                        <button class="btn btn-small">+Tenho</button>
+                    </li>
+                    <li>
+                        <button class="btn btn-small">Enviar</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
