98edd419f340a2118ffdafbec31e72a3b50d4853
lucasmessiaspg <lucasmessias.pg@outlook.com>
2016-08-31 00:56:31 -0300

    layout card

--- resources/views/frontend/card/card.blade.php
+++ resources/views/frontend/card/card.blade.php
@@ -1,2 +1,19 @@
-<div class="info-card"></div>
-<div class="img-card"></div>
+<div class="info-card">
+    <div>
+        <h4 class="center-align"><a>Nome da carta (#12/123)</a></h4>
+        <ul>
+            <li>Coleção: <strong>Fate Colide</strong></li>
+            <li>Raridade: <strong>Uncomon</strong></li>
+            <li>Trainer: <a>Messias</a></li>
+            <li>PokePoint: <strong>1313</strong></li>
+        </ul>
+        <ul class="button">
+            <li><button class="btn btn-small">+Desejo</button></li>
+            <li><button class="btn btn-small">+Tenho</button></li>
+            <li><button class="btn btn-small">Enviar</button></li>
+        </ul>
+    </div>
+</div>
+<div class="img-card valign-wrapper">
+    <img class="valign materialboxed" src="/img/40.png" alt="">
+</div>
