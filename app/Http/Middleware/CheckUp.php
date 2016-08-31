a78de39c98ee06e8488062278415eb3b32a556f2
lucasmessiaspg <lucasmessias.pg@outlook.com>
2016-08-31 01:23:15 -0300

    layout card

---
+++ app/Http/Middleware/CheckUp.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Requests\Request;
+use Closure;
+use Illuminate\Console\Application;
+
+class CheckUp
+{
+    protected $request;
+    protected $app;
+
+    public function __construct(Application $app, Request $request)
+    {
+        $this->app = $app;
+        $this->request = $request;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($this->app->isDownForMaintenance() &&
+            !in_array($this->request->getClientIp(), ['86.10.190.248', '86.4.7.24']))
+        {
+            return response('Be right back!', 503);
+        }
+
+        return $next($request);
+    }
+}
