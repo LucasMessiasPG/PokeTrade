53a1d6ac7634c846172d8b9603e6854f77949351
LucasMessaisPG <lucasmessias.pg@outlook.com>
2016-10-06 16:40:31 -0300

    teste travis 33

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,2 +1,4 @@
+composer install
+npm i
 gulp
 sudo chmod 777 -R /var/www/html/PokeTrade
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,3 @@
 sudo apt-get install nodejs
 sudo apt-get install nodejs-legacy
-composer install
-npm i
 sudo npm i -g gulp
