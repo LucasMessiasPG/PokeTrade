752a1076729fb15b7f672eabf547539c18398b5d
LucasMessiasPG <lucasmessias.pg@outlook.com>
2016-10-20 19:49:50 -0200

    add materialize

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,7 @@ elixir(function (mix) {
         .copy('node_modules/angular', 'public/vendor')
         .copy('node_modules/angular-route', 'public/vendor')
         .copy('node_modules/angular-sanitize', 'public/vendor')
+        .copy('node_modules/materialize-css/dist', 'public/vendor/materialize')
         // .copy('node_modules/bootstrap-sass/assets/stylesheets/', 'resources/vendor/bootstrap')
         .copy('node_modules/jquery/dist/jquery.js', 'public/vendor')
         .copy('resources/views/html', 'public/html')
